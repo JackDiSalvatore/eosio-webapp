087e8e80ad9c2c6caa8c168b7b715e2919ed0333
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-06-16 09:22:45 -0400

    removing not needed quotation marks

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -35,39 +35,39 @@ class App extends Component {
         "chain_id": "DISCONNECTED",
       },
       accountInfo: {
-        "account_name": "testacc",
-        "core_liquid_balance": "25.8044 EOS",
-        "head_block_num": 1079,
-        "head_block_time": "2018-11-10T00:45:53.500",
-        "privileged": false,
-        "last_code_update": "1970-01-01T00:00:00.000",
-        "created": "2018-11-10T00:37:05.000",
-        "ram_quota": -1,
-        "net_weight": -1,
-        "cpu_weight": -1,
-        "net_limit": { "used": -1, "available": -1, "max": -1 },
-        "cpu_limit": { "used": -1, "available": -1, "max": -1 },
-        "ram_usage": 2724,
-        "permissions": [
+        account_name: "testacc",
+        core_liquid_balance: "25.8044 EOS",
+        head_block_num: 1079,
+        head_block_time: "2018-11-10T00:45:53.500",
+        privileged: false,
+        last_code_update: "1970-01-01T00:00:00.000",
+        created: "2018-11-10T00:37:05.000",
+        ram_quota: -1,
+        net_weight: -1,
+        cpu_weight: -1,
+        net_limit: { used: -1, available: -1, max: -1 },
+        cpu_limit: { used: -1, available: -1, max: -1 },
+        ram_usage: 2724,
+        permissions: [
           { 
-            "perm_name": "active",
-            "parent": "owner",
-            "required_auth": {
-              "accounts": [
+            perm_name: "active",
+            parent: "owner",
+            required_auth: {
+              accounts: [
                 {
-                  "actor": "",
-                  "permission": "",
-                  "weight": 0,
+                  actor: "",
+                  permission: "",
+                  weight: 0,
                 }
               ],
-              "keys": [
+              keys: [
                 {
-                  "key": "",
-                  "weight": 0,
+                  key: "",
+                  weight: 0,
                 }
               ],
-              "threshold": 0,
-              "waits": [
+              threshold: 0,
+              waits: [
                 {
                   "wait_sec": 0,
                   "weight": 0,
@@ -76,25 +76,25 @@ class App extends Component {
             }
           } 
         ],
-        "total_resources": {
+        total_resources: {
           cpu_weight: "10.5481 EOS",
           net_weight: "10.5482 EOS",
           owner: "eosio",
           ram_bytes: 14021,
         },
-        "self_delegated_bandwidth": {
+        self_delegated_bandwidth: {
           cpu_weight: "10.5481 EOS",
           from: "eosio",
           net_weight: "10.5482 EOS",
           to: "eosio"
         },
-        "refund_request": {
+        refund_request: {
           cpu_amount: "0.0000 EOS",
           net_amount: "880.0000 EOS",
           owner: "eosio",
           request_time: "2019-05-15T12:25:29"
         },
-        "voter_info": {
+        voter_info: {
           flags1: 0,
           is_proxy:0,
           last_vote_weight: "6335582760066.02734375000000000",
@@ -128,7 +128,10 @@ class App extends Component {
     console.log('Account Name is ' + accountName);
     try {
       rpc.get_info().then(result => this.setState({chainInfo: result}))
-      rpc.get_account(accountName).then(result => this.setState({accountInfo: result}));
+      rpc.get_account(accountName).then(result => {
+        this.setState({accountInfo: result})
+        console.log(result.permissions)
+      });
       // rpc.get_currency_balance('eosio.token', accountName, 'EOS').then(result => this.setState({ eosBalance: result}));
       rpc.get_table_rows({
        json: true,
