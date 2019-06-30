dd1a351b4b910135cc64466cf853dcc4da497e09
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-06-29 21:48:31 -0400

    using react-circular-progressbar to create the RAM circular progress bar

--- src/components/AccountDetails/RAM.js
+++ src/components/AccountDetails/RAM.js
@@ -4,6 +4,9 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -14,7 +17,16 @@ const styles = theme => ({
   },
   specialTextStyle: {
     color: theme.palette.secondary.main
-  }
+  },
+  progress: {
+    // color: 'radial-gradient(50% 76%, #E424C8 77%, #7A0025 70%)',
+    // color: theme.palette.secondary.main,
+    fontFamily: 'Exo2Regular',
+    height: '10em',
+    width: 'auto',
+    // border: '2px solid #8DFFA9',
+    // boxShadow: '0 0 8px 1px #9200A5',
+  },
 });
 
 class RAM extends Component {
@@ -28,19 +40,34 @@ class RAM extends Component {
         //const { }
         const { classes, accountInfo } = this.props;
 
+        const percentage = Math.floor((accountInfo.ram_usage / accountInfo.ram_quota)*100);
+
         return (
           <Grid container direction="column" spacing={0} className={classes.root}>
-    
-            <Grid item>
-              <Typography className={classes.textStyle}  gutterBottom>
-                { Math.floor((accountInfo.ram_usage / accountInfo.ram_quota)*100) }%
-              </Typography>
-            </Grid>
 
             <Grid item>
-              <Typography className={classes.specialTextStyle} variant="h5">
-                RAM
-              </Typography>
+              <CircularProgressbarWithChildren
+                value={percentage}
+                // text={`${percentage}%`}
+                className={classes.progress}
+                strokeWidth={12}
+                styles={buildStyles({
+                    fontFamily: `Exo2Regular`,
+                    // pathColor: `radial-gradient(50% 76%, #E424C8 77%, #7A0025 70%)`,
+                    pathColor: `#E424C8`,
+                    textColor: `#E424C8`,
+                    trailColor: `#8DFFA9`,
+                    strokeLinecap: 'butt',
+                    textSize: '16px',
+                  })}
+              >
+                <Typography className={classes.textStyle} variant="h6">
+                  {percentage}%
+                </Typography>
+                <Typography className={classes.specialTextStyle} variant="h5">
+                  RAM
+                </Typography>
+              </CircularProgressbarWithChildren>;
             </Grid>
 
             {/* <Grid item>
